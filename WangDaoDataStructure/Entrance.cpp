065f07ae8b0a724b4164a4f55a911acb417326d0
zhangjun <zhangjun@cadg.cn>
2020-05-18 13:06:17 +0800

    .

--- WangDaoDataStructure/Entrance.cpp
+++ WangDaoDataStructure/Entrance.cpp
@@ -1,17 +1,17 @@
 #include"2.2SqList.h"
-#include"Utils/DataUtils.h"
+#include"Utils/ListUtils.h"
 int main() {
 	SqList* sql = InitSqList();
 	for (int i = 0; i < 25; i++)
 	{
-		sql->data[i] = i % 5;
+		sql->data[i] = i;
 	}
-	/*sql->data[25] = -1;
+	sql->data[25] = 25;
 	for (int i = 26; i < MaxSize; i++)
 	{
 		sql->data[i] = i;
-	}*/
+	}
 	sql->length = 50;
-	DeleteValueX(sql, 3);
+	int pos = Bisearch(sql->data, 50, 26);
 	return 0;
 }
