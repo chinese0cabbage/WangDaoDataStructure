a0ebcaaccab9f3693003c562c80ec95616cd95c2
zhangjun <zhangjun@cadg.cn>
2020-06-03 14:14:50 +0800

    迁移折半查找的核心代码到SearchUtils

--- WangDaoDataStructure/Utils/ListUtils.h
+++ WangDaoDataStructure/Utils/ListUtils.h
@@ -1,22 +1,6 @@
 #pragma once
 #include"DataUtils.h"
 
-//二分查找double型有序数组中的goal的索引位置，如若不存在，返回-1
-int Bisearch(double arr[], double goal, int high = 0, int low = 0) {//tested
-	int mid = (low + high) / 2;
-	while (high >= low)
-	{
-		mid = (low + high) / 2;
-		if (arr[mid] < goal)
-			low = mid + 1;
-		else if (arr[mid] == goal)
-			return mid;
-		else
-			high = mid - 1;
-	}
-	return -1;
-}
-
 //逆序下标在low和high之间的元素（包含low和high）
 void ReversedLimitOrder(double arr[], int low, int high) {//tested
 	while (high > low) {
--- WangDaoDataStructure/Utils/SearchUtils.h
+++ WangDaoDataStructure/Utils/SearchUtils.h
@@ -2,6 +2,22 @@
 #include<malloc.h>
 #include"TypeUtils.h"
 
+//二分查找double型有序数组中的goal的索引位置，如若不存在，返回-1
+int Bisearch(double arr[], double goal, int high = 0, int low = 0) {//tested
+	int mid = (low + high) / 2;
+	while (high >= low)
+	{
+		mid = (low + high) / 2;
+		if (arr[mid] < goal)
+			low = mid + 1;
+		else if (arr[mid] == goal)
+			return mid;
+		else
+			high = mid - 1;
+	}
+	return -1;
+}
+
 //查找不包含在有序数组中的元素在哪两个元素之间，返回这两个元素的下标，item2为较大的的下标,如若当前元素的值大于数组的最大值，则返回<-1,length-1>，若小于最小值，则返回<0,-1>
 Tuple* SearchAmongSubscript(double arr[], int length, double goal) {//tested
 	int high = length - 1, low = 0, mid = (high + low) / 2;
