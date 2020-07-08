8baefce914e5ad00daee905ca1a9a3ad73df9709
zhangjun <zhangjun@cadg.cn>
2020-07-08 11:25:27 +0800

    添加链表的头尾插入节点的方法

--- WangDaoDataStructure/Utils/LinkedListUtils.h
+++ WangDaoDataStructure/Utils/LinkedListUtils.h
@@ -3,12 +3,26 @@
 #include<malloc.h>
 #include"ErrorUtils.h"
 
-void InsertNode2Head(LinkedListNode* linkedList, double goal) {
+LinkedListNode* InsertNode2Head(LinkedListNode* linkedList, double goal) {
+	LinkedListNode* newNode = (LinkedListNode*)malloc(sizeof(LinkedListNode));
+	if (!newNode)
+		ErrorExit(MallocError);
+	newNode->val = goal;
+	newNode->next = linkedList;
+	return newNode;
+}
+
+void InsertNode2Tail(LinkedListNode* linkedList, double goal) {
 	LinkedListNode* newNode = (LinkedListNode*)malloc(sizeof(LinkedListNode));
 	LinkedListNode* head = linkedList;
 	if (!newNode)
 		ErrorExit(MallocError);
+	if (!head) {
+		return;
+	}
 	newNode->val = goal;
-	newNode->next = head;
-	linkedList = newNode;
+	newNode->next = NULL;
+	while (head->next)
+		head = head->next;
+	head->next = newNode;
 }
